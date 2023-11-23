f712c66af790582a65ccb8f26179ed1af979513e
Shadid <shadidshadman@hotmail.com>
2023-11-22 19:14:07 -0800

    added working react code for 3 openweather api features, no node_modules so please run npm install before building

---
+++ open-weather-apis/src/components/ForecastDisplay.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const ForecastDisplay = ({ forecastData }) => {
+  if (!forecastData) {
+    return null; // Don't render anything if the data is not available
+  }
+  return (
+  <div>
+    <h2>Forecast (next 5 days)</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Temperature</th>
+            <th>Weather</th>
+            <th>Wind Speed</th>
+            <th>Cloudiness</th>
+            <th>Probability of Precipitation</th>
+            <th>Rain Volume</th>
+            <th>Snow Volume</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecastData && forecastData.map((data, index) => {
+            const forecastTime = new Date(data.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+            return (
+              <tr key={index}>
+                <td>{new Date(data.dt * 1000).toLocaleDateString()}</td>
+                <td>{forecastTime}</td>
+                <td>{Math.round(data.main.temp)}°C</td>
+                <td>{data.weather[0].description}</td>
+                <td>{data.wind.speed} m/s</td>
+                <td>{data.clouds.all}%</td>
+                <td>{data.pop}%</td>
+                <td>{data.rain ? data.rain['3h'] : 0} mm</td>
+                <td>{data.snow ? data.snow['3h'] : 0} mm</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+  </div>
+);
+};
+
+export default ForecastDisplay;
