a5cbf4b0e1fe74b271e6c383cfb2328501aed30f
Shadid <shadidshadman@hotmail.com>
2023-11-23 18:08:05 -0800

    51 - updated weather widget style as 5-day forecast not workable in React

--- open-weather API/src/components/WeatherWidget.js
+++ open-weather API/src/components/WeatherWidget.js
@@ -2,33 +2,27 @@ import React, { useEffect } from 'react';
 
 const WeatherWidget = ({ cityId }) => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js";
-    document.body.appendChild(script);
-
     if (!window.myWidgetParam) {
       window.myWidgetParam = [];
     }
 
     window.myWidgetParam.push({
-      id: 11,
+      id: 15,
       cityid: cityId,
       appid: '463bce9969af3f76b2c3f7ddecd37e80',
       units: 'metric',
-      containerid: `openweathermap-widget-${cityId}`,
+      containerid: `openweathermap-widget-15-${cityId}`,
     });
 
-    const widgetScript = document.createElement('script');
-    widgetScript.async = true;
-    widgetScript.charset = "utf-8";
-    widgetScript.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-    document.body.appendChild(widgetScript);
+    const script = document.createElement('script');
+    script.async = true;
+    script.charset = "utf-8";
+    script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+    document.body.appendChild(script);
   }, [cityId]);
 
   return (
-    <>
-      <div id={`openweathermap-widget-${cityId}`} />
-    </>
+    <div id={`openweathermap-widget-15-${cityId}`} />
   );
 };
 
