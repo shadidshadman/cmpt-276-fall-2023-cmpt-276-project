6281b47576344d2e15d09813fbbd64200e2e4d5a
Michaela Foo <99845325+mgfoo@users.noreply.github.com>
2023-11-26 17:15:24 -0800

    committing amadeus

--- travel-c-h/src/App.js
+++ travel-c-h/src/App.js
@@ -26,7 +26,6 @@ const App = () => {
         setAirPollutionData={setAirPollutionData} 
       />
     </div>
-    
   );
 }
 
--- travel-c-h/src/weather.js
+++ travel-c-h/src/weather.js
@@ -5,13 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const rootElement = document.getElementById('weatherSearch');
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
