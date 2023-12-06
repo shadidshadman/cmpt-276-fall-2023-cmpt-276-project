fd40a9330b80fdf67646c84c89d22c47b613b583
Shadid <shadidshadman@hotmail.com>
2023-12-05 22:35:26 -0800

    updated weather key

--- travel-c-h/src/components/AllData.js
+++ travel-c-h/src/components/AllData.js
@@ -3,16 +3,18 @@ import { useEffect } from 'react';
 import axios from 'axios';
 
 const AllData = ({ city, setWeatherData, setForecastData, setAirPollutionData }) => {
+  const appid = '463bce9969af3f76b2c3f7ddecd37e80';
+  
   useEffect(() => {
     if (city) {
       axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=463bce9969af3f76b2c3f7ddecd37e80`)
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}`)
         .then((response) => {
           const { lat, lon } = response.data.coord;
           setWeatherData(response.data);
 
           axios
-            .get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=463bce9969af3f76b2c3f7ddecd37e80&units=metric`)
+            .get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`)
             .then((response) => {
               setForecastData(response.data.list);
             })
@@ -21,7 +23,7 @@ const AllData = ({ city, setWeatherData, setForecastData, setAirPollutionData })
             });
 
           axios
-            .get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=463bce9969af3f76b2c3f7ddecd37e80`)
+            .get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${appid}`)
             .then((response) => {
               setAirPollutionData(response.data.list[0]);
             })
