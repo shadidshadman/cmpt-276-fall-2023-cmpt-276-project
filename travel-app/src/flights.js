63d65209004e73a122e2bb9a2df9fadeb23626d3
Dodo20987 <79189913+Dodo20987@users.noreply.github.com>
2023-12-13 12:02:02 -0800

    Updated variable names from apikey to apiID to hopefully not get revoked.

--- travel-app/src/flights.js
+++ travel-app/src/flights.js
@@ -7,15 +7,15 @@ import "react-datepicker/dist/react-datepicker.css";
 var global_token;
 const getToken = async () => {
     const apiUrl = 'https://api.amadeus.com/v1/security/oauth2/token';
-    const apikey = "8Scu4JiUEJnquAgpDbqyIOZjMHQOgAyD";
-    const apisecret = "SNQCPQb5a80UeCqj";
+    const apiID = "8Scu4JiUEJnquAgpDbqyIOZjMHQOgAyD";
+    const apiPass = "SNQCPQb5a80UeCqj";
     try {
       const response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: `grant_type=client_credentials&client_id=${apikey}&client_secret=${apisecret}`,
+        body: `grant_type=client_credentials&client_id=${apiID}&client_secret=${apiPass}`,
       });
   
       if (response.ok) {
